05b6c12d08959ed3efaa7560ca57f3c6784f0666
Jason King <jason.peters.king@gmail.com>
2019-12-07 21:15:45 +0000

    add add-cake page + controller

--- src/main/java/com/waracle/cakemgr/cake/CakeWebController.java
+++ src/main/java/com/waracle/cakemgr/cake/CakeWebController.java
@@ -2,7 +2,11 @@ package com.waracle.cakemgr.cake;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CakeWebController {
@@ -17,5 +21,17 @@ public class CakeWebController {
     public String homePage(Model model) {
         model.addAttribute("cakes", repository.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = "/addCake", method = RequestMethod.GET)
+    public String addCake(Model model) {
+        model.addAttribute("cake", new Cake());
+        return "add-cake";
+    }
+
+    @RequestMapping(value = "/saveCake", method = RequestMethod.POST)
+    public String saveCake(@ModelAttribute Cake cake, BindingResult errors, Model model) {
+        repository.save(cake);
+        return homePage(model);
     }
 }
