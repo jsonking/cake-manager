d494bbd15f41492b0465c633ddec5529772eb526
Jason King <jason.peters.king@gmail.com>
2019-12-08 10:44:30 +0000

    Add additional tests

--- src/test/java/com/waracle/cakemgr/cake/CakeWebControllerTest.java
+++ src/test/java/com/waracle/cakemgr/cake/CakeWebControllerTest.java
@@ -11,9 +11,13 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import static java.lang.String.format;
 
 @RunWith(SpringRunner.class)
@@ -32,5 +36,26 @@ public class CakeWebControllerTest {
                 format("http://localhost:%d/",port), String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         assertTrue("cakes text was missing from homepage",entity.getBody().contains("Cakes"));
+    }
+
+    @Test
+    public void testAddCakeRequestReturnsAddPage() {
+        ResponseEntity<String> entity = restTemplate.getForEntity(
+                format("http://localhost:%d/addCake",port), String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertTrue("add cakes text was missing from homepage",entity.getBody().contains("Add Cake"));
+    }
+
+    @Test
+    public void testSaveCakeIsSuccessful_AndShownOnPage() {
+
+        MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+        parts.add("name", "test cake1");
+        parts.add("description", "a test cake to be saved");
+        parts.add("imageURL", "http://image_url");
+
+        ResponseEntity<String> entity = restTemplate.postForEntity(format("http://localhost:%d/saveCake", port), parts, String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertTrue("test cake was not added",entity.getBody().contains("test cake1"));
     }
 }
